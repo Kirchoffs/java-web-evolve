Kirchoffs/JavaWebEvolve

Basic web framework demo

src/main/java/org/syh/service/impl/DemoServiceImpl.java
@@ -1,0 +1,15 @@
+package org.syh.service.impl;
+
+import org.syh.dao.DemoDao;
+import org.syh.dao.impl.DemoDaoImpl;
+import org.syh.service.DemoService;
+
+import java.util.List;
+
+public class DemoServiceImpl implements DemoService {
+    private DemoDao demoDao = new DemoDaoImpl();
+    @Override
+    public List<String> findAll() {
+        return demoDao.findAll();
+    }
+}

Kirchoffs/java-web-evolve

src/main/java/org/syh/service/impl/DemoServiceImpl.java
@@ -1,0 +1,15 @@
+package org.syh.service.impl;
+
+import org.syh.dao.DemoDao;
+import org.syh.dao.impl.DemoGinkgoDaoImpl;
+import org.syh.service.DemoService;
+
+import java.util.List;
+
+public class DemoServiceImpl implements DemoService {
+    private DemoDao demoDao = new DemoGinkgoDaoImpl();
+    @Override
+    public List<String> findAll() {
+        return demoDao.findAll();
+    }
+}